7ca63ef8c5a8bea3a3f66cf5279cd91bbeea0202
Shoji <faketurn@gmail.com>
2018-01-15 14:15:18 +0900

    input and output

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,10 +1,15 @@
-/*
+    
+        /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package javaapplication1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+    
 /**
  *
  * @author samantha
@@ -15,10 +20,20 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here あいうえおd漢漢字
-        System.out.print("こんにちは ");
-        System.out.println("こんにちは");
-
+        System.out.println("お名前を入力してください");
+        String name = inputName();
+        System.out.println("こんにちは" + name + "さん！");
+    }
+    private static String inputName() {
+        String name = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            name = in.readLine();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+        return name;
     }
     
 }
