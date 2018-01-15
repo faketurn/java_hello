fe63cf23f435690cd095669e50130b14fd80762f
Shoji <faketurn@gmail.com>
2018-01-15 13:44:26 +0900

    fix message

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here あいうえおd漢漢字
-        System.out.println("こんにちは");
+        System.out.print("こんにちは ");
         System.out.println("こんにちは");
 
     }
